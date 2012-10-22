115f05fce254c713b08d574f550cf8776fec42be
brainster-one <brainster-one@hotmail.com>
2012-10-22 21:52:42 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itblog::Application.config.secret_token = '6ba15c5e9945a5c253e6d3052e5777c9b5e58f8353a3b344a4f9afee096822769731962223cbf738ac93d6714985d68b3bfb8cf65347ca820e9f2d78ff177186'
